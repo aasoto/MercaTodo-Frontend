10c3685488564963925c82f4ef0d1c32b78556cc
aasoto <andresalfredosotosuarez@gmail.com>
2023-08-18 22:39:30 -0500

    feat: Create filter in showcase

--- src/ui/modules/showcase/ShowcasePage.jsx
+++ src/ui/modules/showcase/ShowcasePage.jsx
@@ -1,30 +1,52 @@
 import { useEffect, useState } from 'react'
 import { Container, PageTitle, Paginate, PrimaryInfoXL } from '../../components'
-import { Showcase } from '../../../classes';
+import { Generics, Showcase } from '../../../classes';
 import { ShowcaseProductCard } from './ShowcaseProductCard';
 import { ENV } from '../../../../env';
+import { useForm } from '../../../hooks';
 
 export const ShowcasePage = () => {
 
     const { APIUrl, endPoints, parameters } = ENV;
-    const { apiVersion, showcase } = endPoints;
+    const { apiVersion, showcase, products } = endPoints;
 
     const [response, setResponse] = useState({ loading: true });
+
+    const [categories, setCategories] = useState([]);
+
+    const {search, price, category, onInputChange} = useForm({
+        search: '', 
+        price: '',
+        category: '',
+    });
+
     const [pageUrl, setPageUrl] = useState(
         localStorage.getItem('lastEndpoint')
         ? localStorage.getItem('lastEndpoint')
         : `${APIUrl}${apiVersion}${showcase.index}?include=${parameters.showcase.include}`
     );
-
+    
     const showcaseRequest = new Showcase();
 
     useEffect(() => {
-        showcaseRequest.getData(pageUrl)
+        
+        const genericRequest = new Generics();
+
+        // Get products categories
+        genericRequest.getData(`${APIUrl}${apiVersion}${products.categories.index}`)
+            .then(resp => {
+                setCategories(resp.data);
+            });
+        
+    }, []);
+
+    useEffect(() => {
+        showcaseRequest.getData(`${pageUrl}&filter[name]=${search}&filter[price]=${price}&filter[products_category_id]=${category}`)
             .then(resp => {
                 localStorage.setItem('lastEndpoint', pageUrl);
                 setResponse({ loading: false, ...resp });
             });
-    }, [pageUrl]);
+    }, [pageUrl, search, price, category]);
 
     return (
         <Container>
@@ -32,6 +54,49 @@ export const ShowcasePage = () => {
                 Vitrina de productos
             </PageTitle>
             <hr className="border" />
+            <div className="w-full bg-white dark:bg-gray-800 rounded-md p-4 shadow-md">
+                <div className="grid grid-cols-4 gap-4">
+                    <div className='col-span-2'>
+                        <input 
+                            type="text" 
+                            name='search'
+                            className="border border-gray-500 rounded-md px-7 py-3 placeholder:italic w-full"
+                            placeholder="Buscar"
+                            value={search}
+                            onChange={onInputChange}
+                        />
+                    </div>
+                    <div className='col-span-1'>
+                        <input 
+                            type="number" 
+                            name='price'
+                            className="border border-gray-500 rounded-md px-5 py-3 placeholder:italic w-full"
+                            placeholder="Precio"
+                            value={price}
+                            onChange={onInputChange}
+                        />
+                    </div>
+                    <div className='col-span-1'>
+                        <select
+                            name="category"
+                            className="border border-gray-500 rounded-md px-5 py-3 placeholder:italic w-full"
+                            value={category}
+                            onChange={onInputChange}
+                        >
+                            <option value="">Seleccionar...</option>
+                            {   categories.length != 0 &&
+                                categories.map(category => {
+                                    return (
+                                        <option key={category.id} value={category.id}>
+                                            {category.name}
+                                        </option>
+                                    );
+                                })
+                            }
+                        </select>
+                    </div>
+                </div>
+            </div>
             {
                 response.loading
                     ? <PrimaryInfoXL>Cargando...</PrimaryInfoXL>
